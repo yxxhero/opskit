f81ba87712f465d0522577280386aadd3e547aec
root <root@localhost.localdomain>
2018-09-17 02:07:03 +0800

    样式修改

--- opskit/src/App.js
+++ opskit/src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'antd';
+import logo from './logo.png'
+import { Form, Button, Layout, Menu, Breadcrumb } from 'antd';
+import { TopTips } from './component/toptips/toptips'
 
+const { Header, Footer, Content } = Layout;
+const FormItem = Form.Item;
 class App extends Component {
   render() {
     return (
-      <div className="App">
-			<Button type="primary">Button</Button>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     <Layout className="layout">
+       <TopTips  />
+       <Header>
+         <div className="logo">
+			<img src={logo} alt="" style={{width: 120}}/>
+         </div>
+         <Menu
+           theme="dark"
+           mode="horizontal"
+           defaultSelectedKeys={['2']}
+           style={{ display: "inline-block", lineHeight: '64px' }}
+         >
+           <Menu.Item key="1">web服务</Menu.Item>
+           <Menu.Item key="2">数据库</Menu.Item>
+           <Menu.Item key="3">安全</Menu.Item>
+         </Menu>
+			 <Form layout="inline" style={{margin: "12px auto", float: "right"}}>
+        		<FormItem style={{marginLeft: 16, marginRight: 0}}>
+        		  <Button
+        		    type="primary"
+        		  >
+					注册
+        		  </Button>
+        		</FormItem>  
+        		<FormItem style={{marginLeft: 16, marginRight: 0}}>
+        		  <Button
+        		    type="primary"
+        		  >
+					登录
+        		  </Button>
+        		</FormItem>  
+			 </Form>
+       </Header>
+       <Content style={{ padding: '0 50px' }}>
+         <Breadcrumb style={{ margin: '16px 0' }}>
+           <Breadcrumb.Item>Home</Breadcrumb.Item>
+           <Breadcrumb.Item>List</Breadcrumb.Item>
+           <Breadcrumb.Item>App</Breadcrumb.Item>
+         </Breadcrumb>
+         <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+       </Content>
+       <Footer style={{ textAlign: 'center' }}>
+         Ant Design ©2018 Created by Ant UED
+       </Footer>
+     </Layout>
     );
   }
 }
---
+++ opskit/src/component/toptips/toptips.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './toptips.css'
+
+class TopTips extends Component {
+
+    render(){
+	    return (
+		<div className="toptips">
+		    <div className="block">
+				<div style={{color: "white"}}>
+			        专注于运维垂直搜索 运维自动化 更好的解决运维问题 服务运维人员	
+		        </div>
+		    </div>
+		</div>
+		)
+	
+	}
+
+}
+
+
+export { TopTips }
