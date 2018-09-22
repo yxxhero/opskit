7374d64236ed71fdab8af03c34fdce7cb85b91dd
root <root@localhost.localdomain>
2018-09-23 01:59:06 +0800

    bug fix

--- opskit/config-overrides.js
+++ opskit/config-overrides.js
@@ -1,9 +1,16 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireEslint = require('react-app-rewire-eslint');
+
+function overrideEslintOptions(options) {
+  // do stuff with the eslint options...
+  return options;
+}
 
 module.exports = function override(config, env) {
    config = injectBabelPlugin(
        ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
        config
        ) 
+   config = rewireEslint(config, env, overrideEslintOptions);
   return config;
 };
