f3c2f3f54d0a3f709cc393a41455121e7c6635ac
yxxhero <root@localhost.localdomain>
2018-12-22 22:42:25 +0800

    修改顶部宣传语

--- opskit/src/component/toptips/toptips.js
+++ opskit/src/component/toptips/toptips.js
@@ -9,11 +9,9 @@ class TopTips extends Component {
 		<div className="toptips">
 		    <div className="block">
 				<div style={{color: "white", display: "inline-block"}}>
-			        专注于运维垂直搜索 运维自动化 更好的解决运维问题 服务运维人员	
+			        专注于运维垂直 运维自动化 更好的解决运维问题 服务运维人员	
 		        </div>
                 <div style={{float: "right", color: "white"}}>
-                  下载app
-                  <Divider type="vertical" />
                   添加qq群
                   <Divider type="vertical" />
                   关注公众号
